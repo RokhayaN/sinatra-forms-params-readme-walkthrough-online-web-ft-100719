9943dec28427a57584daa26b372bd3bf0dd2d475
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-30 06:46:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,5 +7,10 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-
+ post '/food' do
+  params = {
+  :name => "Sam",
+  :favorite_food => "Green Eggs and Ham"
+}
+ end
 end
